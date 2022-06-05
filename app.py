13cdca97b2adb023de830b5c7ee877062a8d6522
EC2 Default User <ec2-user@ip-172-31-29-128.ec2.internal>
2022-06-05 22:15:30 +0000

    update the host number

--- app.py
+++ app.py
@@ -675,4 +675,4 @@ def test():
 if __name__ == '__main__':
     # Tells Flask to run, accessible from the specified host/port pair. Note
     # that the routes are loaded because of the import above.
-    app.run(host='localhost', port=8083, debug=True)
+    app.run(host='0.0.0.0', port=8083, debug=True)
